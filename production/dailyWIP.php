1690224b9a6ee1d487bb149b2a4707d2a6b333b1
iamnickatuti <atutinick@gmail.com>
2023-06-07 13:03:29 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	production/api/materialFlow.php

--- production/dailyWIP.php
+++ production/dailyWIP.php
@@ -1,60 +1,187 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Filter Array by Date</title>
-    <script src="script.js"></script>
-</head>
-<body>
-<h1>Filter Array by Date</h1>
+<?php
+session_start ();
+include '../parts/header.php';
+?>
+<body style="color: black;">
+<!-- Begin page -->
+<div id="layout-wrapper">
+    <div class="main-content">
+        <header id="page-topbar">
+            <div class="navbar-header">
+                <div class="navbar-brand-box d-flex align-items-left">
+                    <a href="#" class="logo">
+                        <img class="align-middle" src="../assets/logo.svg" height="50px">
+                    </a>
+                    <button type="button" class="btn btn-sm mr-2 font-size-16 d-lg-none header-item waves-effect waves-light" data-toggle="collapse" data-target="#topnav-menu-content">
+                        <i class="fa fa-fw fa-bars"></i>
+                    </button>
+                </div>
+                <div class="d-flex align-items-center">
+                    <div class="dropdown d-inline-block ml-2">
+                        <button type="button" class="btn header-item waves-effect waves-light"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img class="rounded-circle header-profile-user" src="../assets/images/users/pp.png"
+                                 alt="Header Avatar">
+                            <span class="d-none d-sm-inline-block ml-1"><?php if($_SESSION["name"]) { echo $_SESSION["name"];} ?></span>
+                            <i class="mdi mdi-chevron-down d-none d-sm-inline-block"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item d-flex align-items-center justify-content-between" href="../auth/logout.php">
+                                <span>Log Out</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <?php include '../parts/nav.php';?>
 
-<form method="POST" action="">
-    <label for="datepicker">Select a Date:</label>
-    <input type="date" id="datepicker" name="selectedDate">
-    <button type="submit">Filter</button>
-</form>
+        <div class="page-content">
+            <div class="container-fluid">
+                <!-- start page title -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box d-flex align-items-center justify-content-between">
+                            <h4 class="mb-0 font-size-18">
+                                Material Flow 2023
+                            </h4>
+                            <div class="page-title-right">
+                                <span>Price equivalent coming soon</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end row-->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <?php
+                            $json_data = file_get_contents('https://reports.moko.co.ke/production/api/dailyCount.php');
+                            $data = json_decode($json_data, true);
 
-<table id="resultTable">
-    <thead>
-    <tr>
-        <th>Tag</th>
-        <th>Location</th>
-        <th>issuance_team_id</th>
-        <th>Part Number</th>
-        <th>Qty</th>
-        <th>SKU Description</th>
-    </tr>
-    </thead>
-    <tbody>
+                            // Get unique "Tag" and "issuance_team_id" values
+                            $tags = array_unique(array_column($data, 'Tag'));
+                            $issuanceTeams = array_unique(array_column($data, 'issuance_team_id'));
 
-    <?php
-    // Example array of objects
-    $json_data = file_get_contents('https://reports.moko.co.ke/production/api/dailyCount.php');
-    $data = json_decode($json_data, true);
+                            // Filter the data based on the selected "Tag" and "issuance_team_id"
+                            $selectedTag = isset($_GET['tag']) ? $_GET['tag'] : null;
+                            $selectedTeam = isset($_GET['team']) ? $_GET['team'] : null;
 
-    // Check if form is submitted
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // Get selected date from the form
-        $selectedDate = $_POST['selectedDate'];
+                            // Prepare the filtered data for JavaScript usage
+                            $filteredData = array_filter($data, function ($row) use ($selectedTag, $selectedTeam) {
+                                return ($selectedTag === null || $row['Tag'] === $selectedTag)
+                                    && ($selectedTeam === null || $row['issuance_team_id'] === $selectedTeam);
+                            });
+                            $filteredDataJSON = json_encode($filteredData);
+                            ?>
 
-        // Filter the array based on the selected date
-        $filteredData = array_filter($data, function ($item) use ($selectedDate) {
-            return strpos($item['Tag'], $selectedDate) !== false;
-        });
+                            <div class="card-body">
+                                <div class="mb-4">
+                                    <div class="float-right mr-2">
+                                        <select name="team" id="teamSelect" class="form-control form-control-md">
+                                            <option value="">All Teams</option>
+                                            <?php foreach ($issuanceTeams as $team) { ?>
+                                                <option value="<?php echo $team; ?>" <?php echo ($team === $selectedTeam) ? 'selected' : ''; ?>>
+                                                    <?php echo $team; ?>
+                                                </option>
+                                            <?php } ?>
+                                        </select>
+                                    </div>
+                                    <div class="float-right">
+                                        <select name="tag" id="tagSelect" class="form-control form-control-md">
+                                            <option value="">All Tags</option>
+                                            <?php foreach ($tags as $tag) { ?>
+                                                <option value="<?php echo $tag; ?>" <?php echo ($tag === $selectedTag) ? 'selected' : ''; ?>>
+                                                    <?php echo $tag; ?>
+                                                </option>
+                                            <?php } ?>
+                                        </select>
+                                    </div>
 
-        // Display filtered data in the table
-        foreach ($filteredData as $row) {
-            echo "<tr>";
-            echo "<td>" . $row['Tag'] . "</td>";
-            echo "<td>" . $row['Location'] . "</td>";
-            echo "<td>" . $row['issuance_team_id'] . "</td>";
-            echo "<td>" . $row['Part Number'] . "</td>";
-            echo "<td>" . $row['Qty'] . "</td>";
-            echo "<td>" . $row['SKU Description'] . "</td>";
-            echo "</tr>";
-        }
-    }
-    ?>
-    </tbody>
-</table>
+                                    <h5 class="card-title mb-0">Daily Count</h5>
+                                    <br>
+                                    <p class="card-subtitle mb-4">Choose from the drop-downs to display data</p>
+                                </div>
+
+                                <table id="resultTable" class="table table-centered table-striped mb-0">
+                                    <thead>
+                                    <tr>
+                                        <th>Tag</th>
+                                        <th>Location</th>
+                                        <th>issuance_team_id</th>
+                                        <th>Part Number</th>
+                                        <th>Qty</th>
+                                        <th>SKU Description</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody id="tableBody">
+                                    <!-- Display the filtered data in the table -->
+                                    <script>
+                                        var filteredData = <?php echo $filteredDataJSON; ?>;
+
+                                        function updateTable() {
+                                            var tagSelect = document.getElementById('tagSelect');
+                                            var teamSelect = document.getElementById('teamSelect');
+                                            var selectedTag = tagSelect.value;
+                                            var selectedTeam = teamSelect.value;
+
+                                            var filteredRows = filteredData.filter(function(row) {
+                                                return (selectedTag === '' || row.Tag === selectedTag) && (selectedTeam === '' || row.issuance_team_id === selectedTeam);
+                                            });
+
+                                            var tableBody = document.getElementById('tableBody');
+                                            tableBody.innerHTML = '';
+
+                                            filteredRows.forEach(function(row) {
+                                                var tr = document.createElement('tr');
+
+                                                var tdTag = document.createElement('td');
+                                                tdTag.textContent = row.Tag;
+
+                                                var tdLocation = document.createElement('td');
+                                                tdLocation.textContent = row.Location;
+
+                                                var tdTeam = document.createElement('td');
+                                                tdTeam.textContent = row.issuance_team_id;
+
+                                                var tdPartNumber = document.createElement('td');
+                                                tdPartNumber.textContent = row['Part Number'];
+
+                                                var tdQty = document.createElement('td');
+                                                tdQty.textContent = row.Qty;
+
+                                                var tdSkuDescription = document.createElement('td');
+                                                tdSkuDescription.textContent = row['SKU Description'];
+
+                                                tr.appendChild(tdTag);
+                                                tr.appendChild(tdLocation);
+                                                tr.appendChild(tdTeam);
+                                                tr.appendChild(tdPartNumber);
+                                                tr.appendChild(tdQty);
+                                                tr.appendChild(tdSkuDescription);
+                                                tableBody.appendChild(tr);
+                                            });
+                                        }
+
+                                        // Update the table when the dropdowns are changed
+                                        document.getElementById('tagSelect').addEventListener('change', updateTable);
+                                        document.getElementById('teamSelect').addEventListener('change', updateTable);
+
+                                        // Initial table population
+                                        updateTable();
+                                    </script>
+                                    </tbody>
+                                </table>
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
-</html>
+<?php include '../parts/footer.php';?>
--- production/materialFlow.php
+++ production/materialFlow.php
@@ -1,7 +1,6 @@
 <?php
 session_start ();
 include '../parts/header.php';
-include 'sql/sqlOpening.php';
 ?>
 
 <body style="color: black;">
