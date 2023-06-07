2ef262b6135fec44f97da6bbb1863bd5fa0cf1da
iamnickatuti <atutinick@gmail.com>
2023-06-07 13:18:22 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	production/api/materialFlow.php

--- production/api/dailyCount.php
+++ production/api/dailyCount.php
@@ -1,13 +1,14 @@
 <?php
 include '../../cradle_config.php';
 $query = "SELECT
-stocktake_references.date AS 'Tag',
+stocktake_references.tag AS 'Tag',
 locations.name AS 'Location',
 locations.issuance_team_id,
 skus.name AS 'Part Number',
 stocktakes.quantity AS 'Qty',
 skus.description AS 'SKU Description'
 
+  
     FROM
       (((((((((
     cradle.stocktakes 
@@ -21,7 +22,7 @@ skus.description AS 'SKU Description'
     LEFT JOIN sku_types ON sku_types.id = skus.sku_type_id )
     LEFT JOIN units ON units.id = skus.unit_id
     )
-    WHERE stocktake_references.cycle_id = 1 ORDER BY stocktakes.date DESC";
+    WHERE stocktake_references.cycle_id = 1  ORDER BY stocktakes.date DESC";
 
 $data = array();
 
