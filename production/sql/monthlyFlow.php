be45205789d8fd36a783335e2fac8e04c3433f70
iamnickatuti <atutinick@gmail.com>
2023-06-07 11:20:36 +0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	production/api/materialFlow.php

--- production/sql/monthlyFlow.php
+++ production/sql/monthlyFlow.php
@@ -7,33 +7,23 @@ $sqlStocktake = "
       stocktakes.quantity AS 'Qty'
     FROM
       (((((((((
-        cradle.stocktakes
-        LEFT JOIN stocktake_references ON stocktake_references.id = stocktakes.stocktake_reference_id
-      )
-      LEFT JOIN cycles ON cycles.id = stocktake_references.cycle_id
-    )
-    LEFT JOIN stocktake_coverages ON stocktake_coverages.id = stocktake_references.stocktake_coverage_id
-    )
-    LEFT JOIN locations ON locations.id = stocktakes.location_id
-    )
-    LEFT JOIN items ON items.id = stocktakes.item_id
-    )
-    LEFT JOIN skus ON skus.id = stocktakes.sku_id
-    )
-    LEFT JOIN categories ON categories.id = skus.category_id
-    )
-    LEFT JOIN sku_types ON sku_types.id = skus.sku_type_id
-    )
+    cradle.stocktakes 
+    LEFT JOIN stocktake_references ON stocktake_references.id = stocktakes.stocktake_reference_id)
+    LEFT JOIN cycles ON cycles.id = stocktake_references.cycle_id )
+    LEFT JOIN stocktake_coverages ON stocktake_coverages.id = stocktake_references.stocktake_coverage_id )
+    LEFT JOIN locations ON locations.id = stocktakes.location_id )
+    LEFT JOIN items ON items.id = stocktakes.item_id )
+    LEFT JOIN skus ON skus.id = stocktakes.sku_id )
+    LEFT JOIN categories ON categories.id = skus.category_id )
+    LEFT JOIN sku_types ON sku_types.id = skus.sku_type_id )
     LEFT JOIN units ON units.id = skus.unit_id
     )
     WHERE
       cradle.skus.sku_type_id = '1'
       AND locations.id IN ('16', '24')
       AND stocktake_references.cycle_id = 3
-    AND skus.id IN(79, 80, 1810, 1811, 1812, 1813, 1814, 1815, 1816, 1817, 2843, 1818,1819, 1820, 1821, 1823, 1824, 1980, 78, 1804, 1805,
-1806 ,1807, 1808, 1809, 218, 219, 220, 221, 222, 848, 2144, 81, 2451, 75, 970 , 88, 89, 90)
-    ORDER BY
-      stocktakes.date DESC";
+    AND skus.id IN(79, 80, 1810, 1811, 1812, 1813, 1814, 1815, 1816, 1817, 2843, 1818,1819, 1820, 1821, 1823, 1824, 1980, 78, 1804, 1805, 1806 ,1807, 1808, 1809, 218, 219, 220, 221, 222, 848, 2144, 81, 2451, 75, 970 , 88, 89, 90)
+    ORDER BY stocktakes.date DESC";
 
 $cageQuery = "SELECT 
 skus.name as 'Part Name',
@@ -41,8 +31,7 @@ cage_receipts.value AS 'Cages',
 cage_receipts.created_at AS 'Masaa'
 FROM
 (cage_receipts LEFT JOIN skus ON skus.id = cage_receipts.sku_id)
-WHERE skus.id IN(79, 80, 1810, 1811, 1812, 1813, 1814, 1815, 1816, 1817, 2843, 1818,1819, 1820, 1821, 1823, 1824, 1980, 78, 1804, 1805,
-1806 ,1807, 1808, 1809, 218, 219, 220, 221, 222, 848, 2144, 81, 2451, 75, 970 , 88, 89, 90)";
+WHERE skus.id IN(79, 80, 1810, 1811, 1812, 1813, 1814, 1815, 1816, 1817, 2843, 1818,1819, 1820, 1821, 1823, 1824, 1980, 78, 1804, 1805, 1806 ,1807, 1808, 1809, 218, 219, 220, 221, 222, 848, 2144, 81, 2451, 75, 970 , 88, 89, 90)";
 
 $queryYard = "SELECT
     manufacturing_receipts.created_at AS 'Timed',
@@ -57,8 +46,7 @@ FROM
     LEFT JOIN units ON units.id = skus.unit_id)
     LEFT JOIN locations ON locations.id = manufacturing_receipts.location_id)
     LEFT JOIN issuance_teams ON issuance_teams.id = locations.issuance_team_id) 
-    WHERE skus.id IN(79, 80, 1810, 1811, 1812, 1813, 1814, 1815, 1816, 1817, 2843, 1818,1819, 1820, 1821, 1823, 1824, 1980, 78, 1804, 1805,
-1806 ,1807, 1808, 1809, 218, 219, 220, 221, 222, 848, 2144, 81, 2451, 75, 970 , 88, 89, 90)";
+    WHERE skus.id IN(79, 80, 1810, 1811, 1812, 1813, 1814, 1815, 1816, 1817, 2843, 1818,1819, 1820, 1821, 1823, 1824, 1980, 78, 1804, 1805, 1806 ,1807, 1808, 1809, 218, 219, 220, 221, 222, 848, 2144, 81, 2451, 75, 970 , 88, 89, 90)";
 
 
 $sqlConsumption = "SELECT skus.name AS 'Part Name',
@@ -66,5 +54,4 @@ $sqlConsumption = "SELECT skus.name AS 'Part Name',
         block_components.date as 'Tarehe'
         FROM block_components
         LEFT JOIN skus ON skus.id = block_components.sku_id 
-     WHERE skus.id IN(79,  80, 1810, 1811, 1812, 1813, 1814, 1815, 1816, 1817, 2843, 1818,1819, 1820, 1821, 1823, 1824, 1980, 78, 1804, 1805,
-1806 ,1807, 1808, 1809, 218, 219, 220, 221, 222, 848, 2144, 81, 2451, 75, 970 , 88, 89, 90)";
+     WHERE skus.id IN(79,  80, 1810, 1811, 1812, 1813, 1814, 1815, 1816, 1817, 2843, 1818,1819, 1820, 1821, 1823, 1824, 1980, 78, 1804, 1805, 1806 ,1807, 1808, 1809, 218, 219, 220, 221, 222, 848, 2144, 81, 2451, 75, 970 , 88, 89, 90)";
