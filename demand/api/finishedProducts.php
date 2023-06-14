adff30f74cadc2dc961e31193caae55fbba2a685
iamnickatuti <atutinick@gmail.com>
2023-06-14 11:21:46 +0300

    Merge remote-tracking branch 'origin/main'

--- demand/api/finishedProducts.php
+++ demand/api/finishedProducts.php
@@ -1,8 +1,5 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 header('Content-Type: application/json');
 $json1 = file_get_contents('https://reports.moko.co.ke/demand/api/mix.php');
 $json2 = file_get_contents('https://reports.moko.co.ke/demand/api/projection.php');
@@ -13,30 +10,35 @@ $array2 = json_decode($json2, true);
 $result = [];
 // Iterate over each element in $array2
 foreach ($array2 as $item2) {
-    // Check if the "Sub Category" exists in $array1
-    foreach ($array1 as $item1) {
-        if ($item1['Sub-category'] === $item2['Sub Category']) {
-            $multipliedItem = [
-                "Parent Category" => $item2["Parent Category"],
-                "Sub Category" => $item2["Sub Category"],
-                "Part Number" => $item1["Part Number"],
-                "Part Description" => $item1["Part Description"],
-                "UOM" => $item2["UOM"], // Add the unit of measure from json2
-            ];
-            // Multiply the month values by the proportion
-            foreach ($item2 as $key => $value) {
-                if ($key !== "Parent Category" && $key !== "Sub Category" && $key !== "UOM") {
-                    $multipliedItem[$key] = floatval($value) * floatval($item1['proportion']);
-                }
-            }
-            // Append the multiplied item to the result array
-            $result[] = $multipliedItem;
-        }
-    }
+// Check if the "Sub Category" exists in $array1
+$matchFound = false; // Flag to indicate if a match is found
+foreach ($array1 as $item1) {
+if ($item1['Sub-category'] === $item2['Sub Category']) {
+$multipliedItem = [
+"Parent Category" => $item2["Parent Category"],
+"Sub Category" => $item2["Sub Category"],
+"Part Number" => $item1["Part Number"],
+"Part Description" => $item1["Part Description"],
+"UOM" => $item2["UOM"], // Add the unit of measure from json2
+];
+// Multiply the month values by the proportion
+foreach ($item2 as $key => $value) {
+if ($key !== "Parent Category" && $key !== "Sub Category" && $key !== "UOM") {
+$multipliedItem[$key] = floatval($value) * floatval($item1['proportion']);
+}
+}
+// Append the multiplied item to the result array
+$result[] = $multipliedItem;
+$matchFound = true;
+}
+}
+// If no match is found, append the item2 array as is
+if (!$matchFound) {
+$result[] = $item2;
+}
 }
 // Convert the result array to JSON
 $jsonResult = json_encode($result, JSON_PRETTY_PRINT);
 
 // Output the JSON result
 echo $jsonResult;
-?>
