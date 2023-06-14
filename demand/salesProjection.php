f8c928dc412f23f824a6dc729ef5285204aacae1
iamnickatuti <atutinick@gmail.com>
2023-06-14 09:33:12 +0300

    Merge remote-tracking branch 'origin/main'

--- demand/salesProjection.php
+++ demand/salesProjection.php
@@ -1,4 +1,8 @@
 <?php
+session_start ();
+include '../parts/header.php';
+?>
+<?php
 
 // JSON URL
 $jsonUrl = "https://reports.moko.co.ke/demand/api/finishedProducts.php";
@@ -10,23 +14,39 @@ $jsonData = file_get_contents($jsonUrl);
 $data = json_decode($jsonData, true);
 
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>JSON to HTML Table</title>
-    <style>
-        table {
-            border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-        }
-    </style>
-</head>
 <body>
-<table>
+<div id="layout-wrapper">
+    <div class="main-content">
+        <header id="page-topbar">
+            <div class="navbar-header">
+                <div class="navbar-brand-box d-flex align-items-left">
+                    <a href="#" class="logo">
+                        <img class="align-middle" src="../assets/logo.svg" height="50px">
+                    </a>
+                    <button type="button" class="btn btn-sm mr-2 font-size-16 d-lg-none header-item waves-effect waves-light" data-toggle="collapse" data-target="#topnav-menu-content">
+                        <i class="fa fa-fw fa-bars"></i>
+                    </button>
+                </div>
+                <div class="d-flex align-items-center">
+                    <div class="dropdown d-inline-block ml-2">
+                        <button type="button" class="btn header-item waves-effect waves-light"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img class="rounded-circle header-profile-user" src="../assets/images/users/pp.png"
+                                 alt="Header Avatar">
+                            <span class="d-none d-sm-inline-block ml-1"><?php if($_SESSION["name"]) { echo $_SESSION["name"];} ?></span>
+                            <i class="mdi mdi-chevron-down d-none d-sm-inline-block"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item d-flex align-items-center justify-content-between" href="../auth/logout.php">
+                                <span>Log Out</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <?php include '../parts/nav.php';?>
+<table class="table activate-select dt-responsive nowrap" style="font-size: 11px;">
     <thead>
     <tr>
         <?php foreach (array_keys($data[0]) as $header) { ?>
@@ -44,5 +64,7 @@ $data = json_decode($jsonData, true);
     <?php } ?>
     </tbody>
 </table>
+    </div>
+</div>
 </body>
-</html>
+<?php include '../parts/footer.php'; ?>
