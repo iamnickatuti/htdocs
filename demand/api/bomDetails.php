a8bf3acf7be19654ec4da1910b7f962bf0a51ccd
iamnickatuti <atutinick@gmail.com>
2023-06-15 17:32:11 +0300

    Merge remote-tracking branch 'origin/main'

--- demand/api/bomDetails.php
+++ demand/api/bomDetails.php
@@ -7,42 +7,44 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 // The SQL query
 $query = "
-SELECT
+select
   bom_details.bom_id,
-  boms.name AS 'BOM_Name',
-  production_lines.name AS 'Production_Line',
-  skus1.name AS 'Target_sku_Part_Number',
-  skus1.description AS 'Target_sku_Part_Description',
+  boms.name as 'BOM_Name',
+  production_lines.name as 'Production_Line',
+  skus1.name as 'Target_sku_Part_Number',
+  skus1.description as 'Target_sku_Part_Description',
   skus.sku_type_id,
-  skus.name AS 'Component_part_number',
-  skus.description AS 'Component_part_description',
-  bom_details.quantity AS 'component_quantity',
-  units.name AS 'Component_Unit_of_measure',
+  skus.name as 'Component_part_number',
+  skus.description as 'Component_part_description',
+  bom_details.quantity as 'component_quantity',
+  units.name as 'Component_Unit_of_measure',
   bom_details.status,
-  bom_distribution_entries.bom_distribution_id AS 'bom_distribution_id',
-  bom_distribution_entries.share AS '%_bom_share'
-FROM
+  bom_distribution_entries.bom_distribution_id as 'bom_distribution_id',
+  bom_distribution_entries.share as '%_bom_share'
+from
   (
     (
       (
         (
           (
             (
-              bom_details
-              LEFT JOIN skus ON skus.id = bom_details.sku_id
+              (
+                bom_details
+                left join skus on skus.id = bom_details.sku_id
+              )
+              left join boms on boms.id = bom_details.bom_id
             )
-            LEFT JOIN boms ON boms.id = bom_details.bom_id
+            left join production_lines on production_lines.id = boms.production_line_id
           )
-          LEFT JOIN production_lines ON production_lines.id = boms.production_line_id
+          left join skus as skus1 on skus1.id = boms.sku_id
         )
-        LEFT JOIN skus AS skus1 ON skus1.id = boms.sku_id
+        left join units on units.id = skus.unit_id
       )
-      LEFT JOIN units ON units.id = skus.unit_id
+      left join sku_types on sku_types.id = skus.sku_type_id
     )
-    LEFT JOIN sku_types ON sku_types.id = skus.sku_type_id
+    left join bom_distribution_entries on bom_distribution_entries.bom_id = bom_details.bom_id
   )
-  LEFT JOIN bom_distribution_entries ON bom_distribution_entries.bom_id = bom_details.bom_id
-GROUP BY
+group by
   bom_details.bom_id,
   boms.name,
   production_lines.name,
@@ -56,13 +58,15 @@ GROUP BY
   bom_details.status,
   bom_distribution_entries.bom_distribution_id,
   bom_distribution_entries.share
-HAVING
+Having
   bom_distribution_id = (
-    SELECT
-      MAX(bom_distribution_entries.bom_distribution_id)
-    FROM
+    select
+      max(bom_distribution_entries.bom_distribution_id)
+    from
       bom_distribution_entries
-  )";
+      
+      where skus1.description  like 'FP%'
+  );";
 
 // Execute the query
 $result = mysqli_query($conn, $query);
