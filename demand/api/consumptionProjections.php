670945e8f936c8c02f6a5be533fca120bad3008c
iamnickatuti <atutinick@gmail.com>
2023-06-21 11:06:57 +0300

    Merge remote-tracking branch 'origin/main'

--- demand/api/consumptionProjections.php
+++ demand/api/consumptionProjections.php
@@ -6,52 +6,39 @@ global $conn;
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-$json2 = file_get_contents('https://reports.moko.co.ke/demand/api/bomDetails.php');
-$json1 = file_get_contents('https://reports.moko.co.ke/demand/api/components.php');
+$json1 = file_get_contents('https://reports.moko.co.ke/demand/api/bomProkection.php');
+$json2 = file_get_contents('https://reports.moko.co.ke/demand/api/finishedProducts.php');
 $json1Array = json_decode($json1, true);
 $json2Array = json_decode($json2, true);
 
-$products = [];
+$products = $json1Array['products'];
+$jsonOutput = [];
 
-foreach ($json2Array['products'] as $product) {
-    $newProduct = array(
-        'Product' => $product['Product'],
-        'Product_Description' => $product['Product_Description'],
-        'Components' => array()
-    );
+foreach ($products as $product) {
+    $productNumber = $product['Product'];
+    $components = $product['Components'];
 
-    foreach ($product['Components'] as $component) {
-        if (strpos($component['Component_Part_Number'], 'WP') === 0) {
-            // Find subcomponents
-            $subComponents = array();
+    foreach ($json2Array as $item) {
+        if ($item['Part Number'] === $productNumber) {
+            foreach ($components as &$component) {
+                $componentQuantity = (float) $component['Component_Quantity'];
+                $multipliedValues = [];
 
-            foreach ($json1Array as $item) {
-                if ($item['Target_sku_Part_Number'] === $component['Component_Part_Number']) {
-                    $subComponent = array(
-                        'Target_sku_Part_Number' => $item['Target_sku_Part_Number'],
-                        'Target_sku_Part_Description' => $item['Target_sku_Part_Description'],
-                        'Component_part_number' => $item['Component_part_number'],
-                        'Component_part_description' => $item['Component_part_description'],
-                        'component_quantity' => $item['component_quantity'],
-                        'Component_Unit_of_measure' => $item['Component_Unit_of_measure'],
-                        '%_bom_share' => $item['%_bom_share']
-                    );
-
-                    $subComponents[] = $subComponent;
+                foreach ($item as $key => $value) {
+                    if ($key !== 'Part Number' && $key !== 'Part Description' && $key !== 'UOM') {
+                        $multipliedValues[$key] = $value * $componentQuantity;
+                    } else {
+                        $multipliedValues[$key] = $value;
+                    }
                 }
-            }
 
-            $component['Sub_Components'] = $subComponents;
-            $newProduct['Components'][] = $component;
-        } else {
-            $newProduct['Components'][] = $component;
+                $component['Multiplied_Values'] = $multipliedValues;
+            }
         }
     }
 
-    $products[] = $newProduct;
+    $jsonOutput[] = $product;
 }
 
-$result = array('products' => $products);
-$resultJson = json_encode($result, JSON_PRETTY_PRINT);
-echo $resultJson;
-?>
+$output = json_encode($jsonOutput, JSON_PRETTY_PRINT);
+echo $output;
