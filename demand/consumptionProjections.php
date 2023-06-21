c659981fc3ac32e1193f2e4f2f2b26bc6de0055d
iamnickatuti <atutinick@gmail.com>
2023-06-21 15:30:18 +0300

    Merge remote-tracking branch 'origin/main'

--- demand/consumptionProjections.php
+++ demand/consumptionProjections.php
@@ -2,7 +2,39 @@
 session_start ();
 include '../parts/header.php';
 ?>
-
+<body style="color: black;">
+<!-- Begin page -->
+<div id="layout-wrapper">
+    <div class="main-content">
+        <header id="page-topbar">
+            <div class="navbar-header">
+                <div class="navbar-brand-box d-flex align-items-left">
+                    <a href="#" class="logo">
+                        <img class="align-middle" src="../assets/logo.svg" height="50px">
+                    </a>
+                    <button type="button" class="btn btn-sm mr-2 font-size-16 d-lg-none header-item waves-effect waves-light" data-toggle="collapse" data-target="#topnav-menu-content">
+                        <i class="fa fa-fw fa-bars"></i>
+                    </button>
+                </div>
+                <div class="d-flex align-items-center">
+                    <div class="dropdown d-inline-block ml-2">
+                        <button type="button" class="btn header-item waves-effect waves-light"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img class="rounded-circle header-profile-user" src="../assets/images/users/pp.png"
+                                 alt="Header Avatar">
+                            <span class="d-none d-sm-inline-block ml-1"><?php if($_SESSION["name"]) { echo $_SESSION["name"];} ?></span>
+                            <i class="mdi mdi-chevron-down d-none d-sm-inline-block"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <a class="dropdown-item d-flex align-items-center justify-content-between" href="../auth/logout.php">
+                                <span>Log Out</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <?php include '../parts/nav.php';?>
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
