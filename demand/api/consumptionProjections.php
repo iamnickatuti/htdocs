0a99ae26a99df05c6db9a9a909d977fbd584f90a
iamnickatuti <atutinick@gmail.com>
2023-06-21 11:13:42 +0300

    Merge remote-tracking branch 'origin/main'

--- demand/api/consumptionProjections.php
+++ demand/api/consumptionProjections.php
@@ -1,41 +1,56 @@
-        <?php
-        header('Content-Type: application/json');
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-        $json1 = file_get_contents('https://reports.moko.co.ke/demand/api/bomProjection.php');
-        $json2 = file_get_contents('https://reports.moko.co.ke/demand/api/finishedProducts.php');
-        $json1Array = json_decode($json1, true);
-        $json2Array = json_decode($json2, true);
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
-        $products = $json1Array['products'];
-        $jsonOutput = [];
+// Fetch JSON data from URL 1
+$ch1 = curl_init();
+curl_setopt($ch1, CURLOPT_URL, 'https://reports.moko.co.ke/demand/api/bomProjection.php');
+curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
+$json1 = curl_exec($ch1);
+curl_close($ch1);
 
-        foreach ($products as $product) {
-            $productNumber = $product['Product'];
-            $components = $product['Components'];
+// Fetch JSON data from URL 2
+$ch2 = curl_init();
+curl_setopt($ch2, CURLOPT_URL, 'https://reports.moko.co.ke/demand/api/finishedProducts.php');
+curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
+$json2 = curl_exec($ch2);
+curl_close($ch2);
 
-            foreach ($json2Array as $item) {
-                if ($item['Part Number'] === $productNumber) {
-                    foreach ($components as &$component) {
-                        $componentQuantity = (float) $component['Component_Quantity'];
-                        $multipliedValues = [];
+// Decode JSON data
+$json1Array = json_decode($json1, true);
+$json2Array = json_decode($json2, true);
 
-                        foreach ($item as $key => $value) {
-                            if ($key !== 'Part Number' && $key !== 'Part Description' && $key !== 'UOM') {
-                                $multipliedValues[$key] = $value * $componentQuantity;
-                            } else {
-                                $multipliedValues[$key] = $value;
-                            }
-                        }
+$products = $json1Array['products'];
+$jsonOutput = [];
 
-                        $component['Multiplied_Values'] = $multipliedValues;
+foreach ($products as $product) {
+    $productNumber = $product['Product'];
+    $components = $product['Components'];
+
+    foreach ($json2Array as $item) {
+        if ($item['Part Number'] === $productNumber) {
+            foreach ($components as &$component) {
+                $componentQuantity = (float) $component['Component_Quantity'];
+                $multipliedValues = [];
+
+                foreach ($item as $key => $value) {
+                    if ($key !== 'Part Number' && $key !== 'Part Description' && $key !== 'UOM') {
+                        $multipliedValues[$key] = $value * $componentQuantity;
+                    } else {
+                        $multipliedValues[$key] = $value;
                     }
                 }
+
+                $component['Multiplied_Values'] = $multipliedValues;
             }
-
-            $jsonOutput[] = $product;
         }
+    }
 
-        $output = json_encode($jsonOutput, JSON_PRETTY_PRINT);
-        echo $output;
+    $jsonOutput[] = $product;
+}
+
+$output = json_encode($jsonOutput, JSON_PRETTY_PRINT);
+echo $output;
+?>
