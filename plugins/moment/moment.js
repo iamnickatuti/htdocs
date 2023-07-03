7e95398e1a54c09befa5db28938d77bdff7ac8f2
iamnickatuti <atutinick@gmail.com>
2023-07-03 13:27:17 +0300

    Merge remote-tracking branch 'origin/main'

--- plugins/moment/moment.js
+++ plugins/moment/moment.js
@@ -2566,7 +2566,7 @@
         if (model._isUTC) {
             res = model.clone();
             diff = (isMoment(input) || isDate(input) ? input.valueOf() : local__createLocal(input).valueOf()) - res.valueOf();
-            // Use low-level api, because this fn is low-level api.
+            // Use low-level demandapi, because this fn is low-level demandapi.
             res._d.setTime(res._d.valueOf() + diff);
             utils_hooks__hooks.updateOffset(res, false);
             return res;
