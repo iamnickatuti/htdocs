f219d89f2a0176bd55d930f1f9c6721cf970db2b
iamnickatuti <atutinick@gmail.com>
2023-07-03 15:19:34 +0300

    Merge remote-tracking branch 'origin/main'

--- demandapi/rms.php
+++ demandapi/rms.php
@@ -2,149 +2,118 @@
 // Assuming you have a database connection established
 include '../cradle_config.php';
 
-function replaceRawMaterial($data, $bomDistributionId, $conn)
-{
-    $updatedData = $data;
+// Execute the SQL query
+$query = "SELECT
+  production_lines.name AS 'Production_Line',
+  skus1.name AS 'Product',
+  skus1.description AS 'Product Description',
+  CASE
+    WHEN skus.name LIKE 'WP%' THEN sub_raw_materials.name
+    ELSE skus.name
+  END AS 'Raw Material',
+  CASE
+    WHEN skus.name LIKE 'WP%' THEN sub_raw_materials.description
+    ELSE skus.description
+  END AS 'RM Description',
+  bom_details.quantity AS 'Component Quantity',
+  units.name AS 'uom',
+  bom_distribution_entries.share AS '%_BOM_Share'
+FROM
+  bom_details
+  LEFT JOIN boms ON boms.id = bom_details.bom_id
+  LEFT JOIN production_lines ON production_lines.id = boms.production_line_id
+  LEFT JOIN skus AS skus1 ON skus1.id = boms.sku_id
+  LEFT JOIN skus ON skus.id = bom_details.sku_id
+  LEFT JOIN units ON units.id = skus.unit_id
+  LEFT JOIN sku_types ON sku_types.id = skus.sku_type_id
+  LEFT JOIN bom_distribution_entries ON bom_distribution_entries.bom_id = bom_details.bom_id
+  LEFT JOIN skus AS sub_raw_materials ON sub_raw_materials.id = bom_details.sku_id
+WHERE
+  (
+    bom_distribution_entries.bom_distribution_id = (
+      SELECT
+        MAX(bom_distribution_entries.bom_distribution_id)
+      FROM
+        bom_distribution_entries
+      WHERE
+        skus1.description LIKE 'FP%'
+    )
+    OR skus.name LIKE 'WP%'
+  )
+GROUP BY
+  bom_details.bom_id,
+  boms.name,
+  production_lines.name,
+  skus1.name,
+  skus1.description,
+  skus.sku_type_id,
+  skus.name,
+  sub_raw_materials.name,
+  skus.description,
+  sub_raw_materials.description,
+  bom_details.quantity,
+  units.name,
+  bom_details.status,
+  bom_distribution_entries.bom_distribution_id,
+  bom_distribution_entries.share";
 
-    foreach ($updatedData as &$row) {
-        if (strpos($row['Raw Material'], 'WP') === 0) {
-            $rawMaterial = $row['Raw Material'];
+$result = mysqli_query($conn, $query);
 
-            $query = "SELECT skus.name AS 'Raw Material',
-                             skus.description AS 'RM Description',
-                             bom_details.quantity AS 'Component Quantity',
-                             units.name AS 'uom',
-                             bom_distribution_entries.share AS '%_BOM_Share'
-                      FROM bom_details
-                      LEFT JOIN boms ON boms.id = bom_details.bom_id
-                      LEFT JOIN skus ON skus.id = bom_details.sku_id
-                      LEFT JOIN units ON units.id = skus.unit_id
-                      LEFT JOIN bom_distribution_entries ON bom_distribution_entries.bom_id = bom_details.bom_id
-                      WHERE bom_details.bom_id = (SELECT bom_id
-                                                 FROM bom_distribution_entries
-                                                 WHERE bom_distribution_id = ?)
-                        AND skus.description LIKE CONCAT('SRM%', SUBSTRING(?, 3))
-                      GROUP BY bom_details.bom_id, skus.name, skus.description, bom_details.quantity, units.name, bom_details.status, bom_distribution_entries.bom_distribution_id, bom_distribution_entries.share";
+// Fetch the results
+$data = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
 
-            $stmt = mysqli_prepare($conn, $query);
-            mysqli_stmt_bind_param($stmt, 'is', $bomDistributionId, $rawMaterial);
-            mysqli_stmt_execute($stmt);
-            $result = mysqli_stmt_get_result($stmt);
+// Process the data to replace Raw Materials with Sub Raw Materials
+function replaceRawMaterial(&$data, $iteration, $conn) {
+    $rawMaterials = array_column($data, 'Raw Material');
+    $wpRawMaterials = array_filter($rawMaterials, function ($item) {
+        return strpos($item, 'WP') === 0;
+    });
 
-            if ($result) {
-                if ($row = mysqli_fetch_assoc($result)) {
-                    $row['Component Quantity'] *= $updatedData[0]['Component Quantity'];
-                    $row['%_BOM_Share'] *= $updatedData[0]['%_BOM_Share'];
-                    $updatedData[] = $row;
-                }
-            } else {
-                // Display the MySQL error message
-                echo 'MySQL Error: ' . mysqli_error($conn);
-            }
+    if (empty($wpRawMaterials) || $iteration === 5) {
+        return;
+    }
 
-            mysqli_stmt_close($stmt);
+    $wpRawMaterialsString = "'" . implode("', '", $wpRawMaterials) . "'";
+
+    $query = "SELECT
+      skus.name AS 'Raw Material',
+      skus.description AS 'RM Description',
+      sub_raw_materials.name AS 'Sub Raw Material',
+      sub_raw_materials.description AS 'SRM Description'
+    FROM
+      skus
+      INNER JOIN skus AS sub_raw_materials ON sub_raw_materials.id = skus.sub_raw_material_id
+    WHERE
+      skus.name IN ($wpRawMaterialsString)";
+
+    $result = mysqli_query($conn, $query);
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rawMaterial = $row['Raw Material'];
+        $matchingRawMaterials = array_filter($data, function ($item) use ($rawMaterial) {
+            return $item['Raw Material'] === $rawMaterial;
+        });
+
+        foreach ($matchingRawMaterials as &$matchingRawMaterial) {
+            $matchingRawMaterial['Raw Material'] = $row['Sub Raw Material'];
+            $matchingRawMaterial['RM Description'] = $row['SRM Description'];
         }
     }
 
-    return $updatedData;
+    replaceRawMaterial($data, $iteration + 1, $conn);
 }
 
-// Execute Statement 1
-$query1 = "SELECT production_lines.name AS 'Production_Line',
-                  skus1.name AS 'Product',
-                  skus1.description AS 'Product Description',
-                  skus.name AS 'Raw Material',
-                  skus.description AS 'RM Description',
-                  bom_details.quantity AS 'Component Quantity',
-                  units.name AS 'uom',
-                  bom_distribution_entries.share AS '%_BOM_Share',
-                  bom_distribution_entries.bom_distribution_id
-          FROM bom_details
-          LEFT JOIN boms ON boms.id = bom_details.bom_id
-          LEFT JOIN production_lines ON production_lines.id = boms.production_line_id
-          LEFT JOIN skus AS skus1 ON skus1.id = boms.sku_id
-          LEFT JOIN skus ON skus.id = bom_details.sku_id
-          LEFT JOIN units ON units.id = skus.unit_id
-          LEFT JOIN sku_types ON sku_types.id = skus.sku_type_id
-          LEFT JOIN bom_distribution_entries ON bom_distribution_entries.bom_id = bom_details.bom_id
-          GROUP BY bom_details.bom_id, boms.name, production_lines.name, skus1.name, skus1.description, skus.sku_type_id, skus.name, skus.description, bom_details.quantity, units.name, bom_details.status, bom_distribution_entries.bom_distribution_id, bom_distribution_entries.share
-          HAVING bom_distribution_entries.bom_distribution_id = (
-            SELECT MAX(bom_distribution_entries.bom_distribution_id)
-            FROM bom_distribution_entries
-            WHERE skus1.description LIKE 'FP%'
-          )";
+replaceRawMaterial($data, 1, $conn);
 
-$result1 = mysqli_query($conn, $query1);
+// Close the database connection
+mysqli_close($conn);
 
-if ($result1) {
-    // Fetch results from Statement 1
-    $data1 = [];
-    while ($row = mysqli_fetch_assoc($result1)) {
-        $data1[] = $row;
-    }
-
-    // Process the data to recursively replace Raw Materials with Sub Raw Materials
-    $processedData = $data1;
-    $bomDistributionId = $data1[0]['bom_distribution_id'];
-
-    while (count($processedData) > 0) {
-        $processedData = replaceRawMaterial($processedData, $bomDistributionId, $conn);
-    }
-
-    // Close the database connection
-    mysqli_close($conn);
-} else {
-    // Display the MySQL error message
-    echo 'MySQL Error: ' . mysqli_error($conn);
+// Output the data
+foreach ($data as $row) {
+    echo implode(', ', $row) . '<br>';
 }
-
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Result Table</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: left;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
-</head>
-<body>
-<table>
-    <tr>
-        <th>Production Line</th>
-        <th>Product</th>
-        <th>Product Description</th>
-        <th>Raw Material</th>
-        <th>RM Description</th>
-        <th>Component Quantity</th>
-        <th>uom</th>
-        <th>%_BOM_Share</th>
-    </tr>
-    <?php foreach ($processedData as $row): ?>
-        <tr>
-            <td><?php echo $row['Production_Line']; ?></td>
-            <td><?php echo $row['Product']; ?></td>
-            <td><?php echo $row['Product Description']; ?></td>
-            <td><?php echo $row['Raw Material']; ?></td>
-            <td><?php echo $row['RM Description']; ?></td>
-            <td><?php echo $row['Component Quantity']; ?></td>
-            <td><?php echo $row['uom']; ?></td>
-            <td><?php echo $row['%_BOM_Share']; ?></td>
-        </tr>
-    <?php endforeach; ?>
-</table>
-</body>
-</html>
