e3813ded7711c04a6361861f1d33ddc9ea0b1f4e
iamnickatuti <atutinick@gmail.com>
2023-07-03 16:36:08 +0300

    Merge remote-tracking branch 'origin/main'

--- demandapi/rms.php
+++ demandapi/rms.php
@@ -29,6 +29,7 @@ function substituteRawMaterial($conn, $rawMaterial, $depth = 1)
         bom_distribution_entries.bom_distribution_id = (
             SELECT MAX(bom_distribution_entries.bom_distribution_id)
             FROM bom_distribution_entries
+
         )
         AND skus1.name = '" . $rawMaterial . "'
     GROUP BY
@@ -56,7 +57,7 @@ function substituteRawMaterial($conn, $rawMaterial, $depth = 1)
             $subRawMaterial = $row;
 
             // Check if Sub Raw Material starts with WP and depth is less than or equal to 7
-            if (strpos($row['Sub Raw Material'], 'WP') === 0 && $depth <= 8) {
+            if (strpos($row['Sub Raw Material'], 'WP') === 0 && $depth <= 7) {
                 // Call the function recursively to substitute the Sub Raw Material and increment the depth
                 $subSubRawMaterials = substituteRawMaterial($conn, $row['Sub Raw Material'], $depth + 1);
 
@@ -76,14 +77,11 @@ function substituteRawMaterial($conn, $rawMaterial, $depth = 1)
 
 // Execute the SQL query for Products and Raw Materials
 $sqlStatement = "SELECT
-    production_lines.name AS 'Production_Line',
     skus1.name AS 'Product',
-    skus1.description AS 'Product Description',
     skus.name AS 'Raw Material',
     skus.description AS 'RM Description',
     bom_details.quantity AS 'Component Quantity',
-    units.name AS 'uom',
-    bom_distribution_entries.share AS '%_BOM_Share'
+    units.name AS 'uom'
 FROM
     bom_details
 LEFT JOIN skus ON skus.id = bom_details.sku_id
@@ -99,20 +97,14 @@ WHERE
         FROM bom_distribution_entries
         WHERE skus1.description LIKE 'FP%'
     )
+    AND skus.name NOT LIKE 'WP%'
 GROUP BY
     bom_details.bom_id,
-    boms.name,
-    production_lines.name,
     skus1.name,
-    skus1.description,
-    skus.sku_type_id,
     skus.name,
     skus.description,
     bom_details.quantity,
-    units.name,
-    bom_details.status,
-    bom_distribution_entries.bom_distribution_id,
-    bom_distribution_entries.share";
+    units.name";
 
 $result = $conn->query($sqlStatement);
 
@@ -124,15 +116,34 @@ if ($result) {
         $product = $row;
 
         // Check if Raw Material starts with WP
-        if (strpos($row['Raw Material'], 'WP') === 0) {
+        if (strpos($row['Raw Material'], 'WP') !== 0) {
             // Call the function to substitute the Raw Material
             $subRawMaterials = substituteRawMaterial($conn, $row['Raw Material']);
 
             // Add the sub raw materials to the product
-            $product['Sub Raw Materials'] = $subRawMaterials;
+            if (!empty($subRawMaterials)) {
+                foreach ($subRawMaterials as $subRawMaterial) {
+                    $subSubRawMaterials = isset($subRawMaterial['Sub-Sub Raw Materials']) ? $subRawMaterial['Sub-Sub Raw Materials'] : array();
+                    if (empty($subSubRawMaterials)) {
+                        $products[] = array(
+                            'Product' => $product['Product'],
+                            'Raw Material/Sub Raw Material' => $subRawMaterial['Sub Raw Material'],
+                            'RM Description' => $subRawMaterial['SRM Description'],
+                            'Component Quantity' => $subRawMaterial['Component Quantity'],
+                            'uom' => $subRawMaterial['uom']
+                        );
+                    }
+                }
+            } else {
+                $products[] = array(
+                    'Product' => $product['Product'],
+                    'Raw Material/Sub Raw Material' => $product['Raw Material'],
+                    'RM Description' => $product['RM Description'],
+                    'Component Quantity' => $product['Component Quantity'],
+                    'uom' => $product['uom']
+                );
+            }
         }
-
-        $products[] = $product;
     }
 } else {
     echo "Error executing query: " . $conn->error;
@@ -145,71 +156,45 @@ $conn->close();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Product and Raw Material Details</title>
+    <title>Product and Raw Materials</title>
     <style>
         table {
             border-collapse: collapse;
             width: 100%;
         }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+
         th, td {
             padding: 8px;
             text-align: left;
-            border-bottom: 1px solid #ddd;
         }
+
         th {
             background-color: #f2f2f2;
         }
     </style>
 </head>
 <body>
-<h1>Product and Raw Material Details</h1>
 <table>
-    <thead>
     <tr>
-        <th>Production Line</th>
         <th>Product</th>
-        <th>Product Description</th>
-        <th>Raw Material</th>
+        <th>Raw Material/Sub Raw Material</th>
         <th>RM Description</th>
         <th>Component Quantity</th>
-        <th>Unit of Measure</th>
-        <th>% BOM Share</th>
-        <th>Sub Raw Materials</th>
+        <th>uom</th>
     </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($products as $product) : ?>
+    <?php foreach ($products as $product): ?>
         <tr>
-            <td><?php echo $product['Production_Line']; ?></td>
             <td><?php echo $product['Product']; ?></td>
-            <td><?php echo $product['Product Description']; ?></td>
-            <td><?php echo $product['Raw Material']; ?></td>
+            <td><?php echo $product['Raw Material/Sub Raw Material']; ?></td>
             <td><?php echo $product['RM Description']; ?></td>
             <td><?php echo $product['Component Quantity']; ?></td>
             <td><?php echo $product['uom']; ?></td>
-            <td><?php echo $product['%_BOM_Share']; ?></td>
-            <td>
-                <?php if (isset($product['Sub Raw Materials'])) : ?>
-                    <ul>
-                        <?php foreach ($product['Sub Raw Materials'] as $subRawMaterial) : ?>
-                            <li><?php echo $subRawMaterial['Sub Raw Material']; ?></li>
-                            <?php if (isset($subRawMaterial['Sub-Sub Raw Materials'])) : ?>
-                                <ul>
-                                    <?php foreach ($subRawMaterial['Sub-Sub Raw Materials'] as $subSubRawMaterial) : ?>
-                                        <li><?php echo $subSubRawMaterial['Sub Raw Material']; ?></li>
-                                        <!-- Continue nesting for more sub-sub raw materials if needed -->
-                                    <?php endforeach; ?>
-                                </ul>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
-                    </ul>
-                <?php else : ?>
-                    N/A
-                <?php endif; ?>
-            </td>
         </tr>
     <?php endforeach; ?>
-    </tbody>
 </table>
 </body>
 </html>
