639f9ad1246917b4585c82134030a37689103b42
iamnickatuti <atutinick@gmail.com>
2023-07-04 16:06:45 +0300

    Merge remote-tracking branch 'origin/main'

--- demandapi/fronthtml.php
+++ demandapi/fronthtml.php
@@ -68,6 +68,16 @@ $data = json_decode($json_data, true);
                                                             <td><?php echo $sub4_raw_material['Component Quantity']; ?></td>
                                                             <td><?php echo $sub4_raw_material['uom']; ?></td>
                                                         </tr>
+                                                        <?php if (isset($sub4_raw_material['Sub Raw Materials'])): ?>
+                                                            <?php foreach ($sub4_raw_material['Sub Raw Materials'] as $sub5_raw_material): ?>
+                                                                <tr>
+                                                                    <td><?php echo $sub5_raw_material['Raw Material']; ?></td>
+                                                                    <td><?php echo $sub5_raw_material['RM Description']; ?></td>
+                                                                    <td><?php echo $sub5_raw_material['Component Quantity']; ?></td>
+                                                                    <td><?php echo $sub5_raw_material['uom']; ?></td>
+                                                                </tr>
+                                                            <?php endforeach; ?>
+                                                        <?php endif; ?>
                                                     <?php endforeach; ?>
                                                 <?php endif; ?>
                                             <?php endforeach; ?>
