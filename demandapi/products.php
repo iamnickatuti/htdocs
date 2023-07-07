2073aa42c061d3ad901f58a38ca127da559f7b6f
iamnickatuti <atutinick@gmail.com>
2023-07-07 09:42:39 +0300

    Merge remote-tracking branch 'origin/main'

--- demandapi/products.php
+++ demandapi/products.php
@@ -4,6 +4,7 @@ include '../cradle_config.php';
 
 // Execute the SQL query to retrieve the product and raw material details
 $sqlStatement = "SELECT
+    production_lines.name as 'Production_Line',
     skus1.name AS 'Product',
     skus1.description AS 'Product Description',
     skus.name AS 'Raw Material',
@@ -15,6 +16,7 @@ FROM
 LEFT JOIN skus ON skus.id = bom_details.sku_id
 LEFT JOIN boms ON boms.id = bom_details.bom_id
 LEFT JOIN skus AS skus1 ON skus1.id = boms.sku_id
+LEFT JOIN production_lines on production_lines.id = boms.production_line_id
 LEFT JOIN units ON units.id = skus.unit_id
 LEFT JOIN sku_types ON sku_types.id = skus.sku_type_id
 LEFT JOIN bom_distribution_entries ON bom_distribution_entries.bom_id = bom_details.bom_id
@@ -22,7 +24,7 @@ WHERE
     bom_distribution_entries.bom_distribution_id = (
         SELECT MAX(bom_distribution_entries.bom_distribution_id)
         FROM bom_distribution_entries
-        WHERE skus1.name LIKE 'FP%'
+        WHERE skus1.name LIKE 'FP%' AND production_lines.name LIKE 'Mattresses Finishing'
     )
 GROUP BY
     bom_details.bom_id,
