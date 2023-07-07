6979452192580d36e79e8d7e77516f44e8262554
iamnickatuti <atutinick@gmail.com>
2023-07-07 11:47:05 +0300

    Merge remote-tracking branch 'origin/main'

--- test.php
+++ test.php
@@ -19,71 +19,71 @@ $data2 = json_decode($json2, true);
 
 // Recursive function to calculate multiplied values
 function calculateMultipliedValues($rawMaterial, $multipliers, $level = 1) {
-$componentQuantity = $rawMaterial['Component Quantity'];
-$multipliedValues = array();
-foreach ($multipliers as $key => $value) {
-if (strpos($key, '/') !== false) {
-$month = str_replace('/', '', $key);
-$multipliedValue = floatval($componentQuantity) * floatval($value);
-$multipliedValues[$key] = $multipliedValue;
-}
-}
-if ($level < 4 && isset($rawMaterial['Sub Raw Materials']) && is_array($rawMaterial['Sub Raw Materials'])) {
-$subRawMaterials = $rawMaterial['Sub Raw Materials'];
-foreach ($subRawMaterials as &$subRawMaterial) {
-$subRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subRawMaterial, $multipliers, $level + 1);
-if (isset($subRawMaterial['Sub Raw Materials'])) {
-$subSubRawMaterials = $subRawMaterial['Sub Raw Materials'];
-foreach ($subSubRawMaterials as &$subSubRawMaterial) {
-$subSubRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subSubRawMaterial, $multipliers, $level + 2);
-if (isset($subSubRawMaterial['Sub Raw Materials'])) {
-$subSubSubRawMaterials = $subSubRawMaterial['Sub Raw Materials'];
-foreach ($subSubSubRawMaterials as &$subSubSubRawMaterial) {
-$subSubSubRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subSubSubRawMaterial, $multipliers, $level + 3);
-}
-$subSubRawMaterial['Sub Raw Materials'] = $subSubSubRawMaterials;
-}
-}
-$subRawMaterial['Sub Raw Materials'] = $subSubRawMaterials;
-}
-}
-$multipliedValues['Sub Raw Materials'] = $subRawMaterials;
-}
-return $multipliedValues;
+    $componentQuantity = $rawMaterial['Component Quantity'];
+    $multipliedValues = array();
+    foreach ($multipliers as $key => $value) {
+        if (strpos($key, '/') !== false) {
+            $month = str_replace('/', '', $key);
+            $multipliedValue = floatval($componentQuantity) * floatval($value);
+            $multipliedValues[$key] = $multipliedValue;
+        }
+    }
+    if ($level < 4 && isset($rawMaterial['Sub Raw Materials']) && is_array($rawMaterial['Sub Raw Materials'])) {
+        $subRawMaterials = $rawMaterial['Sub Raw Materials'];
+        foreach ($subRawMaterials as &$subRawMaterial) {
+            $subRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subRawMaterial, $multipliers, $level + 1);
+            if (isset($subRawMaterial['Sub Raw Materials'])) {
+                $subSubRawMaterials = $subRawMaterial['Sub Raw Materials'];
+                foreach ($subSubRawMaterials as &$subSubRawMaterial) {
+                    $subSubRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subSubRawMaterial, $multipliers, $level + 2);
+                    if (isset($subSubRawMaterial['Sub Raw Materials'])) {
+                        $subSubSubRawMaterials = $subSubRawMaterial['Sub Raw Materials'];
+                        foreach ($subSubSubRawMaterials as &$subSubSubRawMaterial) {
+                            $subSubSubRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subSubSubRawMaterial, $multipliers, $level + 3);
+                        }
+                        $subSubRawMaterial['Sub Raw Materials'] = $subSubSubRawMaterials;
+                    }
+                }
+                $subRawMaterial['Sub Raw Materials'] = $subSubRawMaterials;
+            }
+        }
+        $multipliedValues['Sub Raw Materials'] = $subRawMaterials;
+    }
+    return $multipliedValues;
 }
 
 // Process data from json2 and update json1
 foreach ($data1 as &$item) {
-$partNumber = $item['Part Number'];
-if (isset($data2[$partNumber])) {
-$rawMaterials = $data2[$partNumber]['Raw Materials'];
-$multipliers = $item;
-foreach ($rawMaterials as &$rawMaterial) {
-$rawMaterial['Multiplied_Values'] = calculateMultipliedValues($rawMaterial, $multipliers);
-if (isset($rawMaterial['Sub Raw Materials'])) {
-$subRawMaterials = $rawMaterial['Sub Raw Materials'];
-foreach ($subRawMaterials as &$subRawMaterial) {
-$subRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subRawMaterial, $multipliers);
-if (isset($subRawMaterial['Sub Raw Materials'])) {
-$subSubRawMaterials = $subRawMaterial['Sub Raw Materials'];
-foreach ($subSubRawMaterials as &$subSubRawMaterial) {
-$subSubRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subSubRawMaterial, $multipliers);
-if (isset($subSubRawMaterial['Sub Raw Materials'])) {
-$subSubSubRawMaterials = $subSubRawMaterial['Sub Raw Materials'];
-foreach ($subSubSubRawMaterials as &$subSubSubRawMaterial) {
-$subSubSubRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subSubSubRawMaterial, $multipliers);
-}
-$subSubRawMaterial['Sub Raw Materials'] = $subSubSubRawMaterials;
-}
-}
-$subRawMaterial['Sub Raw Materials'] = $subSubRawMaterials;
-}
-}
-$rawMaterial['Sub Raw Materials'] = $subRawMaterials;
-}
-}
-$item['Raw Materials'] = $rawMaterials;
-}
+    $partNumber = $item['Part Number'];
+    if (isset($data2[$partNumber])) {
+        $rawMaterials = $data2[$partNumber]['Raw Materials'];
+        $multipliers = $item;
+        foreach ($rawMaterials as &$rawMaterial) {
+            $rawMaterial['Multiplied_Values'] = calculateMultipliedValues($rawMaterial, $multipliers);
+            if (isset($rawMaterial['Sub Raw Materials'])) {
+                $subRawMaterials = $rawMaterial['Sub Raw Materials'];
+                foreach ($subRawMaterials as &$subRawMaterial) {
+                    $subRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subRawMaterial, $multipliers);
+                    if (isset($subRawMaterial['Sub Raw Materials'])) {
+                        $subSubRawMaterials = $subRawMaterial['Sub Raw Materials'];
+                        foreach ($subSubRawMaterials as &$subSubRawMaterial) {
+                            $subSubRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subSubRawMaterial, $multipliers);
+                            if (isset($subSubRawMaterial['Sub Raw Materials'])) {
+                                $subSubSubRawMaterials = $subSubRawMaterial['Sub Raw Materials'];
+                                foreach ($subSubSubRawMaterials as &$subSubSubRawMaterial) {
+                                    $subSubSubRawMaterial['Multiplied_Values'] = calculateMultipliedValues($subSubSubRawMaterial, $multipliers);
+                                }
+                                $subSubRawMaterial['Sub Raw Materials'] = $subSubSubRawMaterials;
+                            }
+                        }
+                        $subRawMaterial['Sub Raw Materials'] = $subSubRawMaterials;
+                    }
+                }
+                $rawMaterial['Sub Raw Materials'] = $subRawMaterials;
+            }
+        }
+        $item['Raw Materials'] = $rawMaterials;
+    }
 }
 
 // Convert the updated data to JSON
@@ -91,3 +91,4 @@ $outputData = json_encode($data1, JSON_PRETTY_PRINT);
 
 // Output the JSON data
 echo $outputData;
+
