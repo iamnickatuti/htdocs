3ad9d35a2656e211cc73db9ffc28d9dc0c67b9cf
iamnickatuti <atutinick@gmail.com>
2023-07-10 07:50:09 +0300

    Merge remote-tracking branch 'origin/main'

--- demandapi/matresses/test.php
+++ demandapi/matresses/test.php
@@ -6,8 +6,8 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 // JSON URLs
-$json1Url = "https://reports.moko.co.ke/demandapi/projmix.php";
-$json2Url = "https://reports.moko.co.ke/demandapi/processed.json";
+$json1Url = "https://reports.moko.co.ke/demandapi/matresses/projmix.php";
+$json2Url = "https://reports.moko.co.ke/demandapi/matresses/processed.json";
 
 // Retrieve JSON data
 $json1 = file_get_contents($json1Url);
