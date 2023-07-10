72413677c8e63433e4acf0e9baa381bc20f100d3
iamnickatuti <atutinick@gmail.com>
2023-07-10 08:18:40 +0300

    Merge remote-tracking branch 'origin/main'

--- demandapi/mattresses-consumption.php
+++ demandapi/mattresses-consumption.php
@@ -47,13 +47,13 @@ include '../parts/header.php';
     <div class="row">
         <div class="col-12">
             <div class="page-title-box d-flex align-items-center justify-content-between">
-                <h4 class="mb-0 font-size-18">Queue Management</h4>
+                <h4 class="mb-0 font-size-18">Matteresses Projection</h4>
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
                         <li class="breadcrumb-item">
                             <a href="javascript: void(0); ">MoKo
                             </a></li>
-                        <li class="breadcrumb-item active">Queue Management</li>
+                        <li class="breadcrumb-item active">Matteresses Projection</li>
                     </ol>
                 </div>
             </div>
