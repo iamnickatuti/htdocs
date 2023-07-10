16c05a4f4328f3e81308da02439b92439a6ccd37
iamnickatuti <atutinick@gmail.com>
2023-07-10 07:42:23 +0300

    Merge remote-tracking branch 'origin/main'

--- demandapi/matresses/consumption.php
+++ demandapi/matresses/consumption.php
@@ -25,7 +25,7 @@ include '../parts/header.php'; ?>
     </thead>
     <tbody>
     <?php
-    $jsonData= file_get_contents('https://reports.moko.co.ke/matresses/test.php');
+    $jsonData= file_get_contents('https://reports.moko.co.ke/demandapi/matresses/test.php');
     // Convert JSON to associative array
     $data = json_decode($jsonData, true);?>
     <?php foreach ($data as $item): ?>
