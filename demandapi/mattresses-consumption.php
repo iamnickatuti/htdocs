aa38f4d5015c20ff8fd9bf91b43def67234a4d28
iamnickatuti <atutinick@gmail.com>
2023-07-11 14:43:03 +0300

    Merge remote-tracking branch 'origin/main'

--- demandapi/mattresses-consumption.php
+++ demandapi/mattresses-consumption.php
@@ -89,56 +89,102 @@ include '../parts/header.php';
         <th>June/2024</th>
     </tr>
     </thead>
-    <tbody>
-    <?php
-    $jsonData= file_get_contents('https://reports.moko.co.ke/demandapi/matresses/test.php');
-    // Convert JSON to associative array
-    $data = json_decode($jsonData, true);?>
-    <?php foreach ($data as $item): ?>
-        <?php foreach ($item['Raw Materials'] as $rawMaterial): ?>
-            <?php if (isset($rawMaterial['Sub Raw Materials'])): ?>
-                <?php foreach ($rawMaterial['Sub Raw Materials'] as $subRawMaterial): ?>
-                    <tr>
-                        <td><?php echo $subRawMaterial['Raw Material']; ?></td>
-                        <td><?php echo $subRawMaterial['RM Description']; ?></td>
-                        <td><?php echo $subRawMaterial['uom']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['July/2023']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['August/2023']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['September/2023']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['October/2023']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['November/2023']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['December/2023']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['January/2024']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['February/2024']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['March/2024']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['April/2024']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['May/2024']; ?></td>
-                        <td><?php echo $subRawMaterial['Multiplied_Values']['June/2024']; ?></td>
-                    </tr>
+            <tbody>
+            <?php
+            $jsonData = file_get_contents('https://reports.moko.co.ke/demandapi/matresses/test.php');
+            // Convert JSON to associative array
+            $data = json_decode($jsonData, true);
+            ?>
+            <?php foreach ($data as $item): ?>
+                <?php foreach ($item['Raw Materials'] as $rawMaterial): ?>
+                    <?php if (isset($rawMaterial['Sub Raw Materials'])): ?>
+                        <?php foreach ($rawMaterial['Sub Raw Materials'] as $subRawMaterial): ?>
+                            <?php if (isset($subRawMaterial['Sub Raw Materials'])): ?>
+                                <?php foreach ($subRawMaterial['Sub Raw Materials'] as $subSubRawMaterial): ?>
+                                    <?php if (isset($subSubRawMaterial['Sub Raw Materials'])): ?>
+                                        <?php foreach ($subSubRawMaterial['Sub Raw Materials'] as $subSubSubRawMaterial): ?>
+                                            <tr>
+                                                <td><?php echo $subSubSubRawMaterial['Raw Material']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['RM Description']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['uom']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['July/2023']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['August/2023']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['September/2023']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['October/2023']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['November/2023']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['December/2023']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['January/2024']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['February/2024']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['March/2024']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['April/2024']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['May/2024']; ?></td>
+                                                <td><?php echo $subSubSubRawMaterial['Multiplied_Values']['June/2024']; ?></td>
+                                            </tr>
+                                        <?php endforeach; ?>
+                                    <?php else: ?>
+                                        <tr>
+                                            <td><?php echo $subSubRawMaterial['Raw Material']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['RM Description']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['uom']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['July/2023']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['August/2023']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['September/2023']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['October/2023']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['November/2023']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['December/2023']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['January/2024']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['February/2024']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['March/2024']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['April/2024']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['May/2024']; ?></td>
+                                            <td><?php echo $subSubRawMaterial['Multiplied_Values']['June/2024']; ?></td>
+                                        </tr>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
+                            <?php else: ?>
+                                <tr>
+                                    <td><?php echo $subRawMaterial['Raw Material']; ?></td>
+                                    <td><?php echo $subRawMaterial['RM Description']; ?></td>
+                                    <td><?php echo $subRawMaterial['uom']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['July/2023']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['August/2023']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['September/2023']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['October/2023']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['November/2023']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['December/2023']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['January/2024']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['February/2024']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['March/2024']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['April/2024']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['May/2024']; ?></td>
+                                    <td><?php echo $subRawMaterial['Multiplied_Values']['June/2024']; ?></td>
+                                </tr>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    <?php else: ?>
+                        <tr>
+                            <td><?php echo $rawMaterial['Raw Material']; ?></td>
+                            <td><?php echo $rawMaterial['RM Description']; ?></td>
+                            <td><?php echo $rawMaterial['uom']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['July/2023']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['August/2023']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['September/2023']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['October/2023']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['November/2023']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['December/2023']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['January/2024']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['February/2024']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['March/2024']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['April/2024']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['May/2024']; ?></td>
+                            <td><?php echo $rawMaterial['Multiplied_Values']['June/2024']; ?></td>
+                        </tr>
+                    <?php endif; ?>
                 <?php endforeach; ?>
-            <?php else: ?>
-                <tr>
-                    <td><?php echo $rawMaterial['Raw Material']; ?></td>
-                    <td><?php echo $rawMaterial['RM Description']; ?></td>
-                    <td><?php echo $rawMaterial['uom']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['July/2023']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['August/2023']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['September/2023']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['October/2023']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['November/2023']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['December/2023']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['January/2024']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['February/2024']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['March/2024']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['April/2024']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['May/2024']; ?></td>
-                    <td><?php echo $rawMaterial['Multiplied_Values']['June/2024']; ?></td>
-                </tr>
-            <?php endif; ?>
-        <?php endforeach; ?>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+            <?php endforeach; ?>
+            </tbody>
+
+        </table>
     </div>
     </div>
     </div>
