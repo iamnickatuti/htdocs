aac753fa5ccfb0a59f2081a5bd6201c8a2ee6e57
iamnickatuti <atutinick@gmail.com>
2023-07-14 12:27:57 +0300

    Merge remote-tracking branch 'origin/main'

--- production/cuttingInfo.php
+++ production/cuttingInfo.php
@@ -47,14 +47,12 @@ include '../parts/header.php';
                             <form method="post" action="">
                                 <div class="row">
                                     <div class="col-4">
-                                    <label for="start_date" style="font-size: 11px">Start Date:</label>
                                     <input type="date" id="start_date" name="start_date" class="form-control" required>
                                     </div>
                                     <div class="col-4">
-                                    <label for="end_date"  style="font-size: 11px">End Date:</label>
                                     <input type="date" id="end_date" name="end_date" class="form-control" required>
                                     </div>
-                                    <div class="col-4 mt-4">
+                                    <div class="col-4">
                                     <input class="btn btn-warning" type="submit" value="Filter">
                                     </div>
                                 </div>
